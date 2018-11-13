eff26bf8e4a3679843a7d1bd4828f89eeb571e89
Nikola Lazarevic <nikola261292@gmail.com>
2018-11-13 18:48:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+   $('.icon').on('click', function() {
+    $('#search').toggleClass('active');
+  });
+	// $(window).on('scroll', function() {
+ //  	if($(document).scrollTop() > 170) {
+ //    	$('#header').addClass('shrink');
+ //    }
+ //    else {
+ //    $('#header').removeClass('shrink');
+ //    }
+ //  });
+	// $('.fa').on('click', function(){
+ //        $('.wrap').toggle('.active');
+ //    });
+});
+
+ 
